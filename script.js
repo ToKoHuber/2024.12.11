52eb42993a37629a8488c864f46374e2ca1ef777
24LP2879 24LP2879 <24LP2879@G304-CC0235.local>
2024-12-11 12:14:03 +0800

    third commit

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 let todos = ["geree tseverleh", "hool hiih", "hicheel hiih", "kino uzeh"];
 const button = document.getElementById("button");
 const input = document.getElementById("input");
-input.addEventListener("input", (event) => {
-  console.log(event.target.value);
-});
+
 button.addEventListener("click", () => {
-  todos.push("asd");
-  renderTodos();
+  if (!input.value) {
+    errorHanding();
+    return;
+  } else {
+    errorRemove();
+    todos.push(input.value);
+    renderTodos();
+    input.value = null;
+  }
 });
 
 function renderTodos() {
   const tasklist = document.getElementById("tasklist");
   tasklist.innerHTML = null;
-  todos.map((todo) => {
+  todos.sort().map((todo) => {
     const p = document.createElement("p");
     p.innerText = todo;
     tasklist.appendChild(p);
   });
+}
+function errorHanding() {
+  const p = document.getElementById("error");
+  p.innerText = "Aldaa garlaa";
+}
+function errorRemove() {
+  const p = document.getElementById("error");
+  p.innerText = null;
 }
